3681870a218c6b2e47e733a31cbb8e0c9526ee85
Daniel Ufeli <danielufeli@yahoo.com>
2022-08-11 07:46:20 +0100

    Push Build Folder

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Home />} />
       </Routes>
     </>
   );
