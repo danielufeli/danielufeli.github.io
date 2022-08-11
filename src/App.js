2dd5e1017bae8d916c2fa3383b6bd17dd92d7244
Daniel Ufeli <danielufeli@yahoo.com>
2022-08-11 06:24:51 +0100

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Brands from './components/Brands';
-import About from './components/About';
-import Skills from './components/Skills';
-import Portfolio from './components/Portfolio';
-import Services from './components/Services';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import BackToTopBtn from './components/BackToTopBtn';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Hero />
-      <Brands />
-      <About />
-      <Skills />
-      <Portfolio />
-      <Services />
-      <Testimonials />
-      <Contact />
-      <Footer />
-      <BackToTopBtn />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 };
